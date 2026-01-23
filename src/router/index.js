68b977bde5da55edcd9f62c27672aedc3a235876
MatejMedvec <matej.medvec@student.ukf.sk>
2026-01-23 15:47:55 +0100

    Use hash router for GitHub Pages refresh

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BlogView from '../views/BlogView.vue'
 import Gallery from '../views/Gallery.vue'
@@ -10,7 +10,7 @@ import ReservationsView from '../views/ReservationsView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
